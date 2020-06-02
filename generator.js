2d4624210d4c0157bfc2d288a502f689205e3943
Jasery <jaserydev@163.com>
2020-06-02 22:30:55 +0800

    feat: add plugin

--- generator.js
+++ generator.js
@@ -1,1 +1,9 @@
-module.exports = (api, options, rootOptions) => { }
+module.exports = (api, options, rootOptions) => {
+  // 修改 `package.json` 里的字段
+  api.extendPackage({
+    devDependencies: {
+      "babel-plugin-lodash": "^3.3.4",
+      "lodash-webpack-plugin": "^0.11.5",
+    }
+  })
+}
--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-module.exports = (api, projectOptions) => { }
+module.exports = (api, projectOptions) => {
+  let lodashPluginOptions = {}
+  if (projectOptions.pluginOptions && projectOptions.pluginOptions.lodash) {
+    lodashPluginOptions = projectOptions.pluginOptions.lodash
+  }
+
+  let { provideEnable = true } = lodashPluginOptions
+
+  api.chainWebpack(webpackConfig => {
+
+    webpackConfig.module
+      .rule('js')
+      .use('babel-loader')
+      .loader('babel-loader')
+      .tap((options = {}) => {
+        options.plugins = [...(options.plugins || []), "lodash"]
+        return options
+      })
+
+    webpackConfig
+      .plugin('lodash-webpack')
+      .use(require('lodash-webpack-plugin'), [])
+
+    if (provideEnable) {
+      const { ProvidePlugin } = require('webpack')
+      webpackConfig.plugin('provide')
+        .use(ProvidePlugin, [{
+          _: 'lodash'
+        }])
+    }
+  })
+}
