e09f65ad21bba835b3d6d5f54afaab054b50903b
Jasery <jaserydev@163.com>
2020-06-17 10:40:47 +0800

    update: devDependencies move to plugin

--- generator.js
+++ generator.js
@@ -1,10 +1,6 @@
 module.exports = (api, options, rootOptions) => {
   // 修改 `package.json` 里的字段
   api.extendPackage({
-    devDependencies: {
-      "babel-plugin-lodash": "^3.3.4",
-      "lodash-webpack-plugin": "^0.11.5",
-    },
     dependencies: {
       "lodash": "^4.17.15",
     }
