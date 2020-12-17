cfe29cf01e2b82056e2c5ae110675658b614c75e
Jasery <jaserydev@163.com>
2020-12-17 10:12:44 +0800

    fix: lodash-webpack-plugin options

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+const defaultLodashWebpackPluginOptions = {
+  shorthands: true,
+  cloning: true,
+  currying: true,
+  caching: true,
+  collections: true,
+  exotics: true,
+  guards: true,
+  metadata: true,
+  deburring: true,
+  unicode: true,
+  chaining: true,
+  memoizing: true,
+  coercions:true,
+  flattening: true,
+  paths:true,
+  placeholders: true
+}
+
 module.exports = (api, projectOptions) => {
   let lodashPluginOptions = {}
   if (projectOptions.pluginOptions && projectOptions.pluginOptions.lodash) {
     lodashPluginOptions = projectOptions.pluginOptions.lodash
   }
 
-  let { provide } = lodashPluginOptions
+  let { provide, webpackPluginOptions } = lodashPluginOptions
 
   api.chainWebpack(webpackConfig => {
 
@@ -17,9 +36,10 @@ module.exports = (api, projectOptions) => {
         return options
       })
 
+    let lodashWebpackPluginOptions = Object.assign({}, webpackPluginOptions, defaultLodashWebpackPluginOptions)
     webpackConfig
       .plugin('lodash-webpack')
-      .use(require('lodash-webpack-plugin'), [])
+      .use(require('lodash-webpack-plugin'), [lodashWebpackPluginOptions])
 
     if (provide) {
       const { ProvidePlugin } = require('webpack')
